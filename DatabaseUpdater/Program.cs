35582ced670593315237e1f418780f8b9148730c
Александр Баскаков <basc11111@gmail.com>
2024-09-13 12:14:00 +0300

    fead: Add cheak args

--- DatabaseUpdater/Program.cs
+++ DatabaseUpdater/Program.cs
@@ -2,6 +2,36 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        if (args.Length == 1)
+        {
+            if (args[0].Contains("-h"))
+            {
+                Console.WriteLine("help");
+            }
+        }
+        else if (args.Length == 2)
+        {
+            var databaseFilePathStr = args[0];
+            var updateSqlFilePathStr = args[1];
+
+            if (!Path.Exists(databaseFilePathStr))
+            {
+                Console.WriteLine("Database file path not exists");
+            }
+
+            if (!Path.Exists(updateSqlFilePathStr))
+            {
+                Console.WriteLine("Update sql file path not exists");
+            }
+
+
+            Console.WriteLine("Hello, World!");
+        }
+        else
+        {
+            Console.WriteLine("help");
+            return;
+        }
     }
 }
