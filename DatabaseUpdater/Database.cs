56cab124134b44f71dde0b2817683f0348f6741d
Александр Баскаков <basc11111@gmail.com>
2024-09-13 13:08:18 +0300

    feat: Add read files

---
+++ DatabaseUpdater/Database.cs
@@ -0,0 +1,8 @@
+﻿namespace DatabaseUpdater;
+
+internal class Database
+{
+    public string Name { get; set; }
+    public string ConnectionString { get; set; }
+    public bool NeedChange { get; set; }
+}
--- DatabaseUpdater/Program.cs
+++ DatabaseUpdater/Program.cs
@@ -1,6 +1,11 @@
-﻿internal class Program
+﻿using DatabaseUpdater;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
 
         if (args.Length == 1)
@@ -12,19 +17,62 @@
         }
         else if (args.Length == 2)
         {
-            var databaseFilePathStr = args[0];
-            var updateSqlFilePathStr = args[1];
+            var databaseFilePath = args[0];
+            var updateSqlFilePath = args[1];
 
-            if (!Path.Exists(databaseFilePathStr))
+            if (!Path.Exists(databaseFilePath))
             {
                 Console.WriteLine("Database file path not exists");
+                return;
             }
 
-            if (!Path.Exists(updateSqlFilePathStr))
+            if (!Path.Exists(updateSqlFilePath))
             {
                 Console.WriteLine("Update sql file path not exists");
+                return;
             }
 
+            var databaseFileJson = "";
+
+            using (var fs = File.OpenRead(databaseFilePath))
+            {
+                byte[] buffer = new byte[fs.Length];
+
+                await fs.ReadAsync(buffer, 0, buffer.Length);
+
+                databaseFileJson = Encoding.Default.GetString(buffer);
+            }
+
+            if (String.IsNullOrEmpty(databaseFileJson))
+            {
+                Console.WriteLine("Database file is empty");
+                return;
+            }
+
+            var databases = JsonSerializer.Deserialize<List<Database>>(databaseFileJson);
+
+            if (databases == null || databases.Count == 0) 
+            {
+                Console.WriteLine("Database file is error");
+                return;
+            }
+
+            var updateSqlStr = "";
+
+            using (var fs = File.OpenRead(updateSqlFilePath))
+            {
+                byte[] buffer = new byte[fs.Length];
+
+                await fs.ReadAsync(buffer, 0, buffer.Length);
+
+                updateSqlStr = Encoding.Default.GetString(buffer);
+            }
+
+            if (String.IsNullOrEmpty(updateSqlStr))
+            {
+                Console.WriteLine("Update sql file is empty");
+                return;
+            }
 
             Console.WriteLine("Hello, World!");
         }
