a8caa9b41eabd65e2293bd9d6b073fcdedaa38db
Александр Баскаков <basc11111@gmail.com>
2024-09-22 23:04:52 +0300

    feat(WORK-11): Update input database file

--- DatabaseUpdater/Program.cs
+++ DatabaseUpdater/Program.cs
@@ -6,6 +6,8 @@ using System.Text.Json;
 
 internal class Program
 {
+    private static string _databaseFilePath = "";
+
     private static async Task Main(string[] args)
     {
         using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
@@ -19,7 +21,108 @@ internal class Program
 Аргументы: 
 [Путь к файлу с базами] [Путь к файлу с sql скриптом]";
 
-        if (args.Length == 1)
+
+        /* Аргументы 
+         * [] -- пустой аргумент
+         * [-f] [путь к файлу с бд] -- путь к файлу с бд
+         */
+
+
+        if (args.Length == 0)
+        { 
+            logger.LogInformation("Введите путь к файлу для подключения к базам данных\nДля выходы введите /q");
+
+            while (String.IsNullOrEmpty(_databaseFilePath))
+            {
+                var databaseFilePath = Console.ReadLine();
+
+                
+                if (databaseFilePath == null || String.IsNullOrEmpty(databaseFilePath.Trim()))
+                {
+                    logger.LogError("Для дальнейшей работы Вы должны обязательно ввести путь к файлу для полдключения к базам данных");
+                    logger.LogInformation("Для выходы введите /q");
+                    continue;
+                }
+
+                if (!Path.Exists(databaseFilePath))
+                {
+                    logger.LogError("Вы указали неверный путь к файлу для полдключения к базам данных");
+                    logger.LogInformation("Для выходы введите /q");
+                    continue;
+                }
+
+                if (databaseFilePath.Equals("/q"))
+                {
+                    return;
+                }
+
+                _databaseFilePath = databaseFilePath;
+            }
+        }
+
+        if(args.Length == 2 && args[0].Equals("-f"))
+        {
+            var databaseFilePath = args[1];
+
+            if (databaseFilePath == null || String.IsNullOrEmpty(databaseFilePath.Trim()))
+            {
+                logger.LogError("Для дальнейшей работы Вы должны обязательно ввести путь к файлу для полдключения к базам данных");
+                return;
+            }
+
+            if (!Path.Exists(databaseFilePath))
+            {
+                logger.LogError("Вы указали неверный путь к файлу для полдключения к базам данных");
+                return;
+            }
+
+            _databaseFilePath = databaseFilePath;
+        }
+
+        var databaseFileJson = "";
+
+        try
+        {
+            using (var fs = File.OpenRead(_databaseFilePath))
+            {
+                byte[] buffer = new byte[fs.Length];
+
+                await fs.ReadAsync(buffer, 0, buffer.Length);
+
+                databaseFileJson = Encoding.Default.GetString(buffer);
+            }
+
+            if (String.IsNullOrEmpty(databaseFileJson))
+            {
+                logger.LogError("Файл для полдключения к базам данных пуст");
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            return;
+        }
+
+        var databases = new List<Database>();
+
+        try
+        {
+            databases = JsonSerializer.Deserialize<List<Database>>(databaseFileJson);
+
+            if (databases == null || databases.Count == 0)
+            {
+                logger.LogError("Не удалось прочитать информацию в файле");
+                return;
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            return;
+        }
+
+        /*if (args.Length == 1)
         {
             if (args[0].Contains("-h"))
             {
@@ -63,7 +166,7 @@ internal class Program
 
             var databases = JsonSerializer.Deserialize<List<Database>>(databaseFileJson);
 
-            if (databases == null || databases.Count == 0) 
+            if (databases == null || databases.Count == 0)
             {
                 logger.LogError("Database file is error");
                 return;
@@ -89,7 +192,7 @@ internal class Program
             foreach (var database in databases)
             {
                 logger.LogInformation($"Start database update: {database.Name}");
-                
+
                 try
                 {
                     await using var dataSource = NpgsqlDataSource.Create(database.ConnectionString);
@@ -98,7 +201,7 @@ internal class Program
 
                     await using var command = new NpgsqlCommand(updateSqlStr, connection, transaction);
                     await command.ExecuteNonQueryAsync();
-                   
+
                     await transaction.CommitAsync();
                     logger.LogInformation($"Finish database update {database.Name}");
                 }
@@ -114,6 +217,6 @@ internal class Program
         {
             logger.LogInformation(helpText);
             return;
-        }
+        }*/
     }
 }
